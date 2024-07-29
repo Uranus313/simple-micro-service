ca5d1a8b1205c30fc1bdfcb242c4e238ca08cd9b
Uranus313 <mehrbodmh82@gmail.com>
2024-07-29 23:57:15 +0330

    adding ingress for multi pathing

--- Job api/index.js
+++ Job api/index.js
@@ -4,7 +4,7 @@ import DBConnection from "./src/DBConnection.js";
 import cors from "cors";
 DBConnection();
 const app = express();
-const port = process.env.PORT || 3001;
+const port = process.env.PORT || 3000;
 
 
 app.get("/",(req,res) => {
--- Job api/src/userAuth.js
+++ Job api/src/userAuth.js
@@ -6,15 +6,16 @@ export async function auth(req,res,next){
     }
     try {
         const decoded = jwt.verify(token,process.env.JWTSECRET);
-        const result = await fetch("http://localhost:3000/users/"+decoded._id)
+        const result = await fetch("http://simplemicroservice:3000/users/"+decoded._id)
         const user = await result.json();
         console.log(user)
-        if(!user){
+        if(!user._id){
             return res.status(401).send("access denied. invalid user.");
         }
         req.user = user;
         next();
     } catch (error) {
+        console.log(error);
         res.status(400).send("invalid token");
     }
 }
