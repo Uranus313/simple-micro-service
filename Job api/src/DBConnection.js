e125e2499ce8a5c243e6533cca43ed371d5ab2bb
Uranus313 <mehrbodmh82@gmail.com>
2024-07-31 20:42:25 +0330

    adding monitoring

--- Job api/src/DBConnection.js
+++ Job api/src/DBConnection.js
@@ -4,11 +4,13 @@ export default function(){
     const password = process.env.MONGO_PASSWORD;
     const MONGO_HOST = process.env.MONGO_HOST || 'localhost'; // Default to localhost
     const MONGO_PORT = process.env.MONGO_PORT || 27017; // Default MongoDB port
-    let dbURL = `mongodb://${username}:${password}@${MONGO_HOST}:${MONGO_PORT}`;
+    // let dbURL = `mongodb://${username}:${password}@${MONGO_HOST}:${MONGO_PORT}`;
+    let dbURL = "mongodb://mongo-0.mongo,mongo-1.mongo,mongo-2.mongo:27017";
+    dbURL += "/sms?authSource=admin&replicaSet=rs0&readPreference=secondaryPreferred"; 
     if (!username){
         dbURL = `mongodb://${MONGO_HOST}:${MONGO_PORT}`;
     }
-    dbURL += "/sms?authSource=admin"; 
+    // dbURL += "/sms?authSource=admin"; 
     console.log(dbURL)
     mongoose.connect(dbURL).then(() => console.log("connected")).catch(err => console.log(err));
 }
--- User api/src/DBConnection.js
+++ User api/src/DBConnection.js
@@ -4,11 +4,13 @@ export default function(){
     const password = process.env.MONGO_PASSWORD;
     const MONGO_HOST = process.env.MONGO_HOST || 'localhost'; // Default to localhost
     const MONGO_PORT = process.env.MONGO_PORT || 27017; // Default MongoDB port
-    let dbURL = `mongodb://${username}:${password}@${MONGO_HOST}:${MONGO_PORT}`;
-    if (!username){
-        dbURL = `mongodb://${MONGO_HOST}:${MONGO_PORT}`;
-    }
-    dbURL += "/sms?authSource=admin"; 
+    // let dbURL = `mongodb://${username}:${password}@${MONGO_HOST}:${MONGO_PORT}`;
+    // if (!username){
+    //     dbURL = `mongodb://${MONGO_HOST}:${MONGO_PORT}`;
+    // }
+    // let dbURL = `mongodb://${username}:${password}@mongo-0.mongo,mongo-1.mongo,mongo-2.mongo:27017`;
+    let dbURL = "mongodb://mongo-0.mongo,mongo-1.mongo,mongo-2.mongo:27017";
+    dbURL += "/sms?authSource=admin&replicaSet=rs0&readPreference=secondaryPreferred"; 
     console.log(dbURL)
     mongoose.connect(dbURL).then(() => console.log("connected")).catch(err => console.log(err));
 }
